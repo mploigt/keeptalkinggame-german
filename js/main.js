8260d67b59670df49b7e767f7f4f80debc3411d4
Maik Ploigt <maik@ploigt.de>
2016-12-28 16:00:50 +0100

    Code besser lesbar eingerückt, dabei fehlende Tags ergänzt und Ressourcen lokal abgelegt.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
 $(document).ready(function () {
-    // Fill in page footers:
-    var pageCount = $(".page").size();
-    pageCount -= $(".no-page-count").size();
-    var currentPage = 0;
+	// Fill in page footers:
+	var page = $(".page");
+	var pageCount = page.size();
+	pageCount -= $(".no-page-count").size();
+	var currentPage = 0;
 	var currentBG = 1;
-    $(".page").each(function (index) {
-        if ($(this).hasClass("no-page-count") == false) {
-            currentPage++;
-            $(".page-footer", $(this)).each(function (index) {
-                if ($(this).hasClass("no-auto-footer") == false) {
-                    $(this).html("Seite " + currentPage + " von " + pageCount);
-                }
-            });
-			
+	page.each(function (index) {
+		if ($(this).hasClass("no-page-count") == false) {
+			currentPage++;
+			$(".page-footer", $(this)).each(function (index) {
+				if ($(this).hasClass("no-auto-footer") == false) {
+					$(this).html("Seite " + currentPage + " von " + pageCount);
+				}
+			});
 			// pages without page count are the main body
 			$(this).addClass('page-bg-0' + currentBG);
-			currentBG++
-			if(currentBG > 7)
-			{
+			currentBG++;
+			if (currentBG > 7) {
 				currentBG = 1;
 			}
-        }
-    });
+		}
+	});
 });
